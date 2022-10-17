d252b949fe6975cc623d29aff443ca1796ccacf9
Jakub Mladý <scrub@gapps.zcu.cz>
2022-10-17 19:41:11 +0200

    forming of protocol

--- client/shit_classes.py
+++ client/shit_classes.py
@@ -1,4 +1,6 @@
+from concurrent.futures import thread
 import socket
+import threading
 from typing import Any, Callable, TypeVar
 Shit_Game = TypeVar("Shit_Game")
 Request = str
@@ -15,16 +17,28 @@ class Shit_Game:
         
         self._serv_sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._serv_sock.connect(serv_info)
+        self._req_handler: threading.Thread = threading.Thread()
+        self._cache = None
+        self._lobbies = None
+
+        self.send_request(self._load_lobby_list, "LOBBY")
         pass
 
-    def send_request(self, data_parser: Callable[[Shit_Game, list[Any]], bool], req: Request) -> bool:
+    def send_request(self, data_parser: Callable[[Shit_Game, list[Any]], bool], req: Request, req_data = None) -> bool:
         if not req in Shit_Game.req_set:
             return False
 
         self._serv_sock.send(req)
+        data_parser(self, self._sock_readline())
         pass
 
-    def _load_cache(self):
+    def _load_lobby_list(self, data):
+        pass
+
+    def _load_cache(self, data):
+        pass
+
+    def _sock_readline(self) -> list[Any]:
         pass
 
     def __serv_send(self):
